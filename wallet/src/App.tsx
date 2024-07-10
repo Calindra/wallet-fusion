d7352761367c26ce1476fea934da74c2a177d0fc
Vinicius Catarino <vinicius.catarino@calindra.com.br>
2024-07-10 14:51:27 -0300

    feat: configure federation on folder wallet

--- wallet/src/App.tsx
+++ wallet/src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Wallet from './components/Wallet'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [anchorEl4, setAnchorEl4] = useState(null);
+
+  // const handleClick4 = (event: any) => {
+  //   setAnchorEl4(event.currentTarget);
+  // };
+
+  const handleClose4 = () => {
+    setAnchorEl4(null);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Wallet handleClose4={handleClose4} anchorEl4={anchorEl4} />
     </>
   )
 }
--- wallet/vite.config.ts
+++ wallet/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "remote_app",
+      filename: "remoteEntry.js",
+      exposes: {},
+      shared: ['react', 'react-dom', '@emotion/react', '@emotion/styled', '@mui/icons-material', '@mui/material']
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
