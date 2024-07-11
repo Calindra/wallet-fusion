04a65c5bfdb83c07482dae52b2ec542051e679d0
Vinicius Catarino <vinicius.catarino@calindra.com.br>
2024-07-11 11:14:22 -0300

    chore: adding config to tsconfig.json

--- wallet/src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
-import './App.css';
-import Greetings from './components/Greetings';
-function App() {
-    return (_jsx(_Fragment, { children: _jsx(Greetings, {}) }));
-}
-export default App;
--- wallet/src/components/Greetings.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Box, Typography } from "@mui/material";
-const Greetings = ({ name }) => {
-    return (_jsx(Box, { sx: {
-            mb: 1
-        }, children: _jsx(Box, { display: "flex", alignItems: "center", children: _jsx(Typography, { variant: "h4", fontWeight: "500", children: name ? `Hello, ${name}` : "Hello" }) }) }));
-};
-export default Greetings;
